731406add2ce3fd30108a630f602ccfab96b80fc
Xavier2a2 <abanjoko@spartaglobal.com>
2021-02-04 21:44:00 +0000

    Initial commit

---
+++ src/Printer.java
@@ -0,0 +1,12 @@
+import java.util.Arrays;
+
+public class Printer {
+
+    public static void printMessage(String s){
+        System.out.println(s);
+    }
+
+    public static void printIntArray(int[] numbers){
+        System.out.println(Arrays.toString(numbers));
+    }
+}
---
+++ src/Sorter.java
@@ -0,0 +1,65 @@
+public interface Sorter {
+
+    int[] sortArray(int[] arrayToSort);
+
+    static int[] bubble_sort(int[] numbers) {
+        int temp;
+        //boolean to check if the list is sorted
+        boolean sorted = false;
+
+        //Iterate through until nothing changes in the iteration
+        while(sorted == false){
+            sorted = true;
+
+            for (int i = 0; i < numbers.length - 1; i++) {
+
+                //If the first number is lower than the second number swap their places and indicate that it isn't sorted
+                if (numbers[i] < numbers[i + 1]) {
+                    temp = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = temp;
+                    sorted = false;
+                }
+
+            }
+        }
+
+        //return the sorted set of numbers
+        return numbers;
+    }
+
+    static int[] bubble_sort(int[] numbers, String direction) {
+        int temp;
+        //boolean to check if the list is sorted
+        boolean sorted = false;
+
+        //Iterate through until nothing changes in the iteration
+        while(sorted == false){
+            sorted = true;
+
+            for (int i = 0; i < numbers.length - 1; i++) {
+
+                //If the first number is lower than the second number swap their places and indicate that it isn't sorted
+                if (numbers[i] < numbers[i + 1]) {
+                    temp = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = temp;
+                    sorted = false;
+                }
+
+            }
+        }
+
+        if(direction.toLowerCase() == "asc" ){
+            int temp2;
+            for(int i = 0; i < numbers.length/2; i++){
+                temp2 = numbers[i];
+                numbers[i] = numbers[numbers.length -1-i];
+                numbers[numbers.length-1-i] = temp2;
+            }
+        }
+
+        //return the sorted set of numbers
+        return numbers;
+    }
+}
